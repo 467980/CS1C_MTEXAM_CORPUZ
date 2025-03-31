9d66f4df990e4cecf428f08b229cc94252a28dc8
467980 <benedickcorpuz274@gmail.com>
2025-03-31 11:57:49 +0800

    Update userprompt.js

--- userprompt.js
+++ userprompt.js
@@ -1,2 +1,7 @@
-# CS1C_MTEXAM_CORPUZ
+//
+let FullName = prompt("please Enter the Fullname");
+
+let Age = prompt("please Enter the Age");
+
+let FavoriteNumber = ("please Enter your FavoriteNumber");
 
